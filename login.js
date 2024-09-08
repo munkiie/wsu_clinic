41ff1fe16f46c10f0635455ee8ff2fe3f80f48c3
munkiie <mokodutlo2003@gmail.com>
2024-09-09 00:13:03 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,1 @@
-function forgotPassword() {
-    const email = prompt('Please enter your email to reset your password:');
-    if (email) {
-        alert(Password reset link has been sent to ${email});
-     }
-        }
+
